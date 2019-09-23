0e7a5aa6e1d1b3d02cb64b5c2a40386ee5953389
chelaruc <chelaruciprian10@gmail.com>
2019-09-23 16:25:05 +0300

    changes to run

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { IndexComponent } from './pages/index/index.component';
+import { ProfilepageComponent } from './pages/examples/profilepage/profilepage.component';
+import { RegisterpageComponent } from './pages/examples/registerpage/registerpage.component';
+import { LandingpageComponent } from './pages/examples/landingpage/landingpage.component';
+
+const routes: Routes =[
+    { path: 'home',                   component: IndexComponent },
+    { path: 'profile',            component: ProfilepageComponent },
+    { path: 'register',           component: RegisterpageComponent },
+    { path: 'landing',            component: LandingpageComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes,{
+      useHash: true
+    })
+  ],
+  exports: [
+  ],
 })
 export class AppRoutingModule { }
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,55 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { BsDropdownModule } from "ngx-bootstrap/dropdown";
+import { ProgressbarModule } from "ngx-bootstrap/progressbar";
+import { TooltipModule } from "ngx-bootstrap/tooltip";
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
+
+import { IndexComponent } from './index/index.component';
+import { ProfilepageComponent } from './examples/profilepage/profilepage.component';
+import { RegisterpageComponent } from './examples/registerpage/registerpage.component';
+import { LandingpageComponent } from './examples/landingpage/landingpage.component';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        BrowserModule,
+        FormsModule,
+        RouterModule,
+        BsDropdownModule.forRoot(),
+        ProgressbarModule.forRoot(),
+        TooltipModule.forRoot(),
+        CollapseModule.forRoot(),
+        TabsModule.forRoot(),
+        PaginationModule.forRoot(),
+        AlertModule.forRoot(),
+        BsDatepickerModule.forRoot(),
+        CarouselModule.forRoot(),
+        ModalModule.forRoot()
+    ],
+    declarations: [
+      IndexComponent,
+      ProfilepageComponent,
+      RegisterpageComponent,
+      LandingpageComponent
+     ],
+    exports:[
+      IndexComponent,
+      ProfilepageComponent,
+      RegisterpageComponent,
+      LandingpageComponent
+     ],
+    providers: []
+})
+export class PagesModule { }
