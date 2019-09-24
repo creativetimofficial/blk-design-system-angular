8d7e6ce7a3be964b6a9de5636c5bb7c2a5198ee5
chelaruc <chelaruciprian10@gmail.com>
2019-09-24 08:29:25 +0300

    remove navbar-transparent on scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer, HostListener, Inject  } from '@angular/core';
+import { Location } from '@angular/common';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'blk-design-system-angular';
+export class AppComponent implements OnInit {
+  constructor( private renderer : Renderer, public location: Location, @Inject(DOCUMENT) document) {}
+  @HostListener('window:scroll', ['$event'])
+    onWindowScroll(e) {
+       if (window.pageYOffset > 550) {
+         var element = document.getElementById('navbar-top');
+         if (element) {
+          element.classList.remove('navbar-transparent');
+         }
+       } else {
+        var element = document.getElementById('navbar-top');
+        if (element) {
+          element.classList.add('navbar-transparent');
+        }
+       }
+    }
+  ngOnInit(){
+    this.onWindowScroll(event);
+  }
 }
