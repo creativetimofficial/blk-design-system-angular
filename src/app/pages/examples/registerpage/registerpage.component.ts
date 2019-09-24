b677cfcf967d862c806d9c1793d649d02bd9c3ae
chelaruc <chelaruciprian10@gmail.com>
2019-09-24 11:48:14 +0300

    finished dashboard

--- src/app/pages/examples/registerpage/registerpage.component.ts
+++ src/app/pages/examples/registerpage/registerpage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-registerpage',
@@ -10,10 +10,36 @@ export class RegisterpageComponent implements OnInit, OnDestroy{
   focus1;
   focus2;
   constructor(){};
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(e) {
+    console.log(e);
+    var squares1 = document.getElementById("square1");
+    var squares2 = document.getElementById("square2");
+    var squares3 = document.getElementById("square3");
+    var squares4 = document.getElementById("square4");
+    var squares5 = document.getElementById("square5");
+    var squares6 = document.getElementById("square6");
+    var squares7 = document.getElementById("square7");
+    var squares8 = document.getElementById("square8");
+
+    var posX = e.clientX - window.innerWidth / 2;
+    var posY = e.clientY - window.innerWidth / 6;
+
+    squares1.style.transform = "perspective(500px) rotateY(" + posX * 0.05 + "deg) rotateX(" + posY * (-0.05) + "deg)";
+    squares2.style.transform = "perspective(500px) rotateY(" + posX * 0.05 + "deg) rotateX(" + posY * (-0.05) + "deg)";
+    squares3.style.transform = "perspective(500px) rotateY(" + posX * 0.05 + "deg) rotateX(" + posY * (-0.05) + "deg)";
+    squares4.style.transform = "perspective(500px) rotateY(" + posX * 0.05 + "deg) rotateX(" + posY * (-0.05) + "deg)";
+    squares5.style.transform = "perspective(500px) rotateY(" + posX * 0.05 + "deg) rotateX(" + posY * (-0.05) + "deg)";
+    squares6.style.transform = "perspective(500px) rotateY(" + posX * 0.05 + "deg) rotateX(" + posY * (-0.05) + "deg)";
+    squares7.style.transform = "perspective(500px) rotateY(" + posX * 0.02 + "deg) rotateX(" + posY * (-0.02) + "deg)";
+    squares8.style.transform = "perspective(500px) rotateY(" + posX * 0.02 + "deg) rotateX(" + posY * (-0.02) + "deg)";
+  }
 
   ngOnInit(){
     var body = document.getElementsByTagName('body')[0];
     body.classList.add('register-page');
+
+    this.onMouseMove(event);
   }
   ngOnDestroy(){
     var body = document.getElementsByTagName('body')[0];
