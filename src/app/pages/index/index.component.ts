3a51f8518815759b448441809779825c30dd3384
chelaruc <chelaruciprian10@gmail.com>
2019-09-24 09:15:43 +0300

    added sliders

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import noUiSlider from "nouislider";
 
 @Component({
   selector: 'app-index',
@@ -11,6 +12,28 @@ export class IndexComponent implements OnInit, OnDestroy{
   ngOnInit(){
     var body = document.getElementsByTagName('body')[0];
     body.classList.add('index-page');
+
+    var slider = document.getElementById("sliderRegular");
+
+    noUiSlider.create(slider, {
+      start: 40,
+      connect: false,
+      range: {
+        min: 0,
+        max: 100
+      }
+    });
+
+    var slider2 = document.getElementById("sliderDouble");
+
+    noUiSlider.create(slider2, {
+      start: [20, 60],
+      connect: true,
+      range: {
+        min: 0,
+        max: 100
+      }
+    });
   }
   ngOnDestroy(){
     var body = document.getElementsByTagName('body')[0];
