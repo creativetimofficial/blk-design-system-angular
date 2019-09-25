ac386186d084ae3fa24d3a53b576c879d8fc7e81
chelaruc <chelaruciprian10@gmail.com>
2019-09-25 15:32:20 +0300

    small changes

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -12,8 +12,11 @@ export class IndexComponent implements OnInit, OnDestroy{
   focus2;
   date = new Date;
   pagination = 3;
+  pagination1 = 1;
   constructor(){};
-
+  scrollToDownload(element: any) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
   ngOnInit(){
     var body = document.getElementsByTagName('body')[0];
     body.classList.add('index-page');
